bc2229a9a3ec7d059c51ca5d9b885e97cb10e54f
andrianarivo <andrianarivo.david@gmail.com>
2023-09-18 18:59:04 +0300

    Router

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Greeting from "../routes/Greeting";
+import store from "../redux/store";
+import {Provider} from "react-redux";
 
 function App() {
-    return (<main>
-        <h1>Rails & React</h1>
-         <p>Hello world</p>
-    </main>);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Greeting/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 export default App;
